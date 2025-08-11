2f315f3804f12e11044a02da63bc29ecffdf0ef3
defaultsamson <stanmoon95@gmail.com>
2025-08-11 13:20:19 +0000

    Change compilation method

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,27 @@
 // The start of the bot main file
 
-console.log("beep boop")
+console.log("beep boop");
+
+import { REST, Routes } from "discord.js";
+
+const TOKEN = "";
+const CLIENT_ID = "807492799623856129";
+
+const commands = [
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+try {
+  console.log("Started refreshing application (/) commands.");
+
+//   await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+  console.log("Successfully reloaded application (/) commands.");
+} catch (error) {
+  console.error(error);
+}
